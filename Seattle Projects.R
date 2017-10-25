dfdd406aea0e9f4a7d7cb3ba89cd3584003f726f
mreyers <matthewreyers3333@gmail.com>
2017-10-25 13:35:49 -0700

    Improving Seattle section

--- Seattle Projects.R
+++ Seattle Projects.R
@@ -14,18 +14,19 @@ CleanerSeattle = function(df){
   df$General.Offense.Number = as.factor(df$General.Offense.Number)
   
   # Convert date time character variables into actual date-times
-  df$Event.Clearance.Date = as.POSIXct(strptime(df$Event.Clearance.Date))
-  df$At.Scene.Time        = as.POSIXct(strptime(df$At.Scene.Time))
+  df$Event.Clearance.Date = as.POSIXct(strptime(df$Event.Clearance.Date, format = "%m/%d/%Y %H:%M:%S"))
+  df$At.Scene.Time        = as.POSIXct(strptime(df$At.Scene.Time, format = "%m/%d/%Y %H:%M:%S"))
   
+  return(df)
 }
 
-range(Seattle$At.Scene.Time)
 
 Seattle = read.csv("~/GitHub/Case-Study-2/Seattle_Police_Department_911_Incident_Response.csv", as.is = TRUE, strip.white = TRUE)
 Cincinnati = read.csv("~/GitHub/Case-Study-2/PDI_Police_Calls_For_Service__CAD_.csv", as.is = TRUE, strip.white = TRUE)
 
-names(Cincinnati)
+Seattle = CleanerSeattle(Seattle)
 
+clean = CleanerSeattle(Seattle)
 View(head(Cincinnati))
 dim(Cincinnati)
 
@@ -85,33 +86,16 @@ x = 1
 
 ################################ SEATTLE BORDER ##########################################
 # Subsets to work with if doing Seattle stuff
-# Seattle1 = Seattle[1:(0.2*length(Seattle$CAD.CDW.ID)),]
-# Seattle2 = Seattle[(0.2*length(Seattle$CAD.CDW.ID)+1): (0.4*length(Seattle$CAD.CDW.ID)),]
-# Seattle3 = Seattle[(0.4*length(Seattle$CAD.CDW.ID)+1): (0.6*length(Seattle$CAD.CDW.ID)),]
-# Seattle4 = Seattle[(0.6*length(Seattle$CAD.CDW.ID)+1): (0.8*length(Seattle$CAD.CDW.ID)),]
-# Seattle5 = Seattle[(0.8*length(Seattle$CAD.CDW.ID)+1): (    length(Seattle$CAD.CDW.ID)),]
-# 
-# dim(Seattle4)
-# write.csv(Seattle1, file = "Seattle1.csv")
-# write.csv(Seattle2, file = "Seattle2.csv")
-# write.csv(Seattle3, file = "Seattle3.csv")
-# write.csv(Seattle4, file = "Seattle4.csv")
-# write.csv(Seattle5, file = "Seattle5.csv")
-
-
-dim(Seattle5)
-dim(Seattle2)
-dim(Seattle1)
-head(Seattle1)
-head(Seattle)
-
-# Check data structure
-str(Seattle)
-
-# Check the number of typos in clearance description
-item = table(Seattle$Event.Clearance.Description)
-item[item < 20]
-item2 = table(Seattle$Event.Clearance.SubGroup)
-item2[item2<2000]
-# Doesn't appear to be any typos
-# Data possibly collected via drop down menus, meaning clean subgroups
+uniq = unique(Seattle$District.Sector)
+uniq = sort(uniq, decreasing = TRUE)
+for(i in 1:length(uniq)){
+  assign(paste("Seattle", uniq[i], sep =""), Seattle[Seattle$District.Sector == uniq[i],])
+}
+names(SeattleM)
+# Seattle map, use to check the area codes I have
+mapSeattle = get_googlemap('Seattle', scale = 2, zoom = 11)
+SeattleMap = ggmap(mapSeattle, extent = "device", legend = "none")
+SeattleMapWithCrime = SeattleMap + geom_point(data = SeattleM, aes(x = Longitude, y = Latitude))
+SeattleMapWithCrime
+SeattleMapWithCrimeLevels = SeattleMap +stat_density2d(aes(x = Longitude, y = Latitude, fill = ..level.., alpha = ..level..),
+                                                       size = 2, bins = 4, geom = "polygon", data = SeattleM)
